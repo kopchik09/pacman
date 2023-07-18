ee0ffcce6d6be4e5044b00587e89105f8c551f17
kopchik09 <kostazo550@gmail.com>
2023-07-18 20:38:35 +1000

    for teacher

--- data/players.py
+++ data/players.py
@@ -5,12 +5,15 @@ class Player(sqlalchemybase):
     id = Column(Integer, primary_key = True, autoincrement = True)
     adress = Column(String)
     name = Column(String(15))
+    color = Column(String(100), default = 'white')
     size = Column(Integer, default = 50)
     x = Column(Integer, default = 500)
     y = Column(Integer, default = 500)
-    speed_y = Column(Integer, default = 0)
-    speed_x = Column(Integer, default = 0)
-    speed_abs = Column(Integer, default = 1)
+    speed_y = Column(Integer, default = 2)
+    speed_x = Column(Integer, default = 2)
+    speed_abs = Column(Integer, default = 2)
+    w_vision = Column(Integer, default = 800)
+    h_vision = Column(Integer, default = 600)
     error = Column(Integer, default = 0)
     def __init__(self, name, adress):
         self.name = name
